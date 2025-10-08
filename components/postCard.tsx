56204709ee89864e5dbd644c5832a0aebdbb0cc0
ycantistudy <ycantistudy@gmail.com>
2025-10-08 08:43:04 +0000

    post card: div -> link

--- components/postCard.tsx
+++ components/postCard.tsx
@@ -15,28 +15,27 @@ export default function PostCard({
   const [hover, setHover] = useState(false)
 
   return (
-    <div
+    <Link
       className={`card bg-base-100 rounded-2xl border shadow-md transition ${
         hover ? "border-primary shadow-lg" : "border-transparent"
       }`}
       onMouseEnter={() => setHover(true)}
       onMouseLeave={() => setHover(false)}
+      href={`/posts/${slug}`}
     >
-      <Link href={`/posts/${slug}`}>
-        <div className="card-body">
-          <h2
-            className={`card-title text-lg font-semibold ${
-              hover ? "underline underline-offset-2" : ""
-            }`}
-          >
-            {title}
-          </h2>
-          <p className="text-sm text-gray-400">
-            {formatDate(date ?? "")} • {readtime} min read
-          </p>
-          <p className="mt-2">{description}</p>
-        </div>
-      </Link>
-    </div>
+      <div className="card-body">
+        <h2
+          className={`card-title text-lg font-semibold ${
+            hover ? "underline underline-offset-2" : ""
+          }`}
+        >
+          {title}
+        </h2>
+        <p className="text-sm text-gray-400">
+          {formatDate(date ?? "")} • {readtime} min read
+        </p>
+        <p className="mt-2">{description}</p>
+      </div>
+    </Link>
   )
 }
